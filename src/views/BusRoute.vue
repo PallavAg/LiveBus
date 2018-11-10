b7bee09d481d3447152ecc58ffbae6a4c079a6bf
Zhixing Zhang <me@neoto.xin>
2018-11-10 17:13:54 -0600

    TheMap

--- src/views/BusRoute.vue
+++ src/views/BusRoute.vue
@@ -1,27 +1,56 @@
 <template>
-  <v-container>
-    <v-layout column>
-      <v-flex>
-        This is where the map goes
-      </v-flex>
-      <v-flex v-if="!route">
-        <v-btn block>Board</v-btn>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div ref="map" id="map" style="height: 100%;">
+    <v-btn
+      tag="div"
+      color="success"
+      class="map-overlay-btn"
+      absolute
+      @click="board"
+    >Board</v-btn>
+  </div>
 </template>
 
 <script>
+import Here, { platform } from "../plugins/heremap"
 export default {
   name: "BusRoute",
   data () {
     return {
       route: null,
     }
+  },
+  mounted () {
+    const defaultLayers = platform.createDefaultLayers()
+    this.map = new Here.Map(
+      this.$refs.map,
+      defaultLayers.normal.map,
+      {
+        zoom: 10,
+        center: { lat: 52.5, lng: 13.4 }
+      }
+    )
+    window.addEventListener('resize', () => {
+      this.map.getViewPort().resize()
+    })
+    const behavior = new Here.mapevents.Behavior(new Here.mapevents.MapEvents(this.map))
+    // const ui = Here.ui.UI.createDefault(this.map, defaultLayers)
+  },
+  methods: {
+    board () {
+
+    }
   }
 }
 </script>
 
-<style scoped>
-
+<style>
+  #map {
+    height: 100%;
+  }
+  .map-overlay-btn {
+    z-index: 100;
+    bottom: 1.5rem;
+    left: 2rem;
+    right: 2rem;
+  }
 </style>
