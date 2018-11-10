74ff8ad3693b521a8f5a8133383640d6c555c705
Hogan Zhou <zhou929@purdue.edu>
2018-11-10 17:43:33 -0600

    merged with maps

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Bus</span>
+  <v-app class="secondary">
+    <v-toolbar class="primary" app>
+      <v-toolbar-title class="mx-0 px-0" v-on:click="home">
+        <v-img
+          :src="require('./assets/bus-side-view.svg')"
+          contain
+          width="55"
+          height="40"
+        ></v-img>
       </v-toolbar-title>
+      <v-toolbar-title class="ml-0 px-0">
+        live-bus
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon v-on:click="login">
+        <v-icon>account_circle</v-icon>
+      </v-btn>
     </v-toolbar>
-
     <v-content>
       <router-view/>
     </v-content>
   </v-app>
 </template>
+<script>
+  export default{
+    methods: {
+      login: function()
+      {
+        this.$router.push('login')
+      },
+      home: function()
+      {
+        this.$router.push('home')
+      }
+    }
+  }
+</script>
 
---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <p class="mt-4 mb-0 text-xs-center headline">sign in</p>
+    <p class="mt-0 mb-0 text-xs-center display-1"><b>live-bus</b></p>
+    <v-img
+      :src="require('../assets/bus-side-view.svg')"
+      contain
+      width="160"
+      height="140"
+      class="mx-auto"
+    ></v-img>
+    <div ref="loginC"></div>
+  </v-container>
+</template>
+
+<script>
+  import { LoadAuthUI } from '@/plugins/firebase'
+  export default {
+    name: "BusRoute",
+    mounted() {
+      LoadAuthUI(this.$refs['loginC'])
+    }
+  }
+</script>
