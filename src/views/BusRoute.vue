374dd8c33274225bba2d3d74fddd7216bc9a36fe
Zhixing Zhang <me@neoto.xin>
2018-11-10 15:07:48 -0600

    Map

--- src/views/BusRoute.vue
+++ src/views/BusRoute.vue
@@ -1,10 +1,24 @@
 <template>
-  <div></div>
+  <v-container>
+    <v-layout column>
+      <v-flex>
+        This is where the map goes
+      </v-flex>
+      <v-flex v-if="!route">
+        <v-btn block>Board</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "BusRoute"
+  name: "BusRoute",
+  data () {
+    return {
+      route: null,
+    }
+  }
 }
 </script>
 
