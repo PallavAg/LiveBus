e0077c438bad812ec7c01954da0a353822ebdcbe
Hogan Zhou <zhou929@purdue.edu>
2018-11-10 14:39:21 -0600

    login

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+    </div>
+</template>
+<script>
+    import { LoadAuthUI } from '@/plugins/firebase'
+    export default {
+        mounted() {
+            LoadAuthUI(this.$el)
+        }
+    }
+</script>
