6dea4ac0baff42998d864f0a29ae47674043e877
Zhixing Zhang <me@neoto.xin>
2018-11-10 14:15:25 -0600

    Firebase Init

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+import '@/plugins/firebase'
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/plugins/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/firestore'
+
+firebase.initializeApp({
+  "apiKey": "AIzaSyADL2pDe43D1r7CCLYqU3Zbe5xSRqYaAn8",
+  "databaseURL": "https://livebus-7dab7.firebaseio.com",
+  "storageBucket": "livebus-7dab7.appspot.com",
+  "authDomain": "livebus-7dab7.firebaseapp.com",
+  "messagingSenderId": "595489917573",
+  "projectId": "livebus-7dab7"
+})
+
+export const db = firebase.firestore()
+export const auth = firebase.auth()
