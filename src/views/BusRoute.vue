0b111c138446db8e228c966012bf896894399a70
Zhixing Zhang <me@neoto.xin>
2018-11-11 09:17:37 -0600

    Clustering Changes Color According To Capacity

--- src/views/BusRoute.vue
+++ src/views/BusRoute.vue
@@ -31,13 +31,57 @@
 <script>
 import Here, {platform} from "../plugins/heremap"
 import firebase, {db, auth} from '@/plugins/firebase'
+const CUSTOM_THEME = {
+  getClusterPresentation (cluster) {
+    let sum = 0
+    let count = 0
+    cluster.forEachDataPoint(datapoint => {
+      const data = datapoint.getData()
+      count += 1
+      if (!data.capacity) {
+        return
+      }
+      sum += data.capacity
+    })
+    const color = ['green', 'yellow', 'red'][Math.round(sum / count)]
+    console.log(color)
+    const markup = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" version="1.1" viewBox="0 0 500 500" fill-rule="evenodd" clip-rule="evenodd">
+<path fill="${color}" d="M112 0l282 0c35,0 62,21 62,56l0 46 51 0c0,25 0,50 0,76l-33 0 0 -59 -18 0 0 275 -80 0 -11 40 91 -1c0,24 -10,45 -37,46l0 22c0,12 -9,22 -21,22l-20 0c-12,0 -21,-10 -21,-22l0 -22 -207 0 0 22c0,12 -10,22 -22,22l-19 0c-12,0 -22,-10 -22,-22l0 -22c-27,-1 -36,-22 -36,-46l91 1 -11 -40 -80 0 0 -275 -18 0 0 59 -33 0c0,-26 0,-51 0,-76l51 0 0 -46c0,-35 27,-56 61,-56zm-30 28c-10,0 -18,7 -18,17l0 263c0,24 20,44 44,44l290 0c24,0 44,-20 44,-44l0 -263c0,-10 -7,-17 -17,-17l-343 0z"/>
+<text x="180" y="280" fill="${color}" font-size="300px" font-weight="bold">${count}</text>
+</svg>`
+    const marker = new Here.map.Marker(cluster.getPosition(), {
+      min: cluster.getMinZoom(),
+      max: cluster.getMaxZoom(),
+      icon: new Here.map.Icon(markup, {
+        size: {w: 30, h: 30},
+        anchor: {x: 15, y: 15}
+      })
+    })
+    return marker
+  },
+  getNoisePresentation (noisePoint) {
+    const data = noisePoint.getData()
+    const noiseMarker = new Here.map.Marker(noisePoint.getPosition(), {
+      min: noisePoint.getMinZoom(),
+      icon: new Here.map.Icon(data.photoURL || 'https://assets-cdn.github.com/images/modules/logos_page/Octocat.png', {
+        size: {w: 20, h: 20},
+        anchor: {x: 10, y: 10}
+      })
+    })
 
+    // Link a data from the point to the marker
+    // to make it accessible inside onMarkerClick
+    noiseMarker.setData(data)
+
+    return noiseMarker
+  }
+}
 export default {
   name: "BusRoute",
   data() {
     return {
       boarded: null,
-      capacity: null,
+      capacity: 0,
       myMarker: null,
     }
   },
@@ -51,7 +95,15 @@ export default {
         center: {lat: 52.5, lng: 13.4}
       }
     )
-    this.clusteredDataProvider = new Here.clustering.Provider([])
+    this.clusteredDataProvider = new Here.clustering.Provider([], {
+      theme: CUSTOM_THEME,
+      clusteringOptions: {
+        // Maximum radius of the neighbourhood
+        eps: 16,
+        // minimum weight of points required to form a cluster
+        minWeight: 1
+      }
+    })
     const layer = new Here.map.layer.ObjectLayer(this.clusteredDataProvider)
     this.map.addLayer(layer)
 
@@ -84,7 +136,6 @@ export default {
           const points = segment.graph.split(' ')
           points.forEach(point => {
             const coords = point.split(',')
-            console.log(coords)
             lineString.pushPoint({lat:coords[0], lng:coords[1]})
           })
           this.map.addObject(new Here.map.Polyline(
@@ -107,9 +158,9 @@ export default {
     docUpdated (doc) {
       const data = doc.data()
       const dataPoints = []
-      console.log(data)
       for (const uid in data.users) {
-        const geoPoint = data.users[uid].location
+        const theUser = data.users[uid]
+        const geoPoint = theUser.location
         const coords = {lat: geoPoint.latitude, lng: geoPoint.longitude}
         if (uid == auth.currentUser.uid) {
           if (this.myMarker) {
@@ -123,7 +174,7 @@ export default {
             this.map.addObject(theMarker)
           }
         } else {
-          dataPoints.push(new Here.clustering.DataPoint(coords.lat, coords.lng))
+          dataPoints.push(new Here.clustering.DataPoint(coords.lat, coords.lng, null, theUser))
         }
 
       }
@@ -139,6 +190,7 @@ export default {
           position.coords.longitude
         )
         obj[`users.${auth.currentUser.uid}.lastAlive`] = new Date()
+        obj[`users.${auth.currentUser.uid}.photoURL`] = auth.currentUser.photoURL
         db.collection('routes').doc(this.route).update(obj)
       } else {
         this.map.setCenter(coords)
