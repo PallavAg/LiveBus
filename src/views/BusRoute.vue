02e1ea99968ed44fabab97babf894852f2d8b99f
Zhixing Zhang <me@neoto.xin>
2018-11-10 18:30:29 -0600

    Write into Firebase

--- src/views/BusRoute.vue
+++ src/views/BusRoute.vue
@@ -12,11 +12,13 @@
 
 <script>
 import Here, { platform } from "../plugins/heremap"
+import firebase, { db, auth } from '@/plugins/firebase'
 export default {
   name: "BusRoute",
   data () {
     return {
-      route: null,
+      route: 'placeholder',
+      boarded: null,
     }
   },
   mounted () {
@@ -37,9 +39,27 @@ export default {
   },
   methods: {
     board () {
-
+      this.boarded = window.navigator.geolocation.watchPosition(this.updateLocation)
+    },
+    updateLocation (position) {
+      const obj = {}
+      obj[`users.${auth.currentUser.uid}.location`] = new firebase.firestore.GeoPoint(
+        position.coords.latitude,
+        position.coords.longitude
+      )
+      db.collection('routes').doc(this.route).update(obj)
+      const svgMarkup = '<svg width="24" height="24" ' +
+        'xmlns="http://www.w3.org/2000/svg">' +
+        '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+        'height="22" /><text x="12" y="18" font-size="12pt" ' +
+        'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+        'fill="white">H</text></svg>';
+      const icon = new Here.map.Icon(svgMarkup)
+      const coords = {lat: position.coords.latitude, lng: position.coords.longitude}
+      const marker = new Here.map.Marker(coords, {icon: icon})
+      this.map.addObject(marker)
     }
-  }
+  },
 }
 </script>
 
