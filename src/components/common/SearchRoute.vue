1e46a39889ce2567269ec35f268c1fec1f66d7de
Prakrit Duangsutha <prakrit_duangsutha@outlook.com>
2018-11-10 23:29:22 -0600

    Added saving to database from landing page

--- src/components/common/SearchRoute.vue
+++ src/components/common/SearchRoute.vue
@@ -4,7 +4,7 @@
     dark
   >
     <v-card-title class="headline red lighten-3" >
-      {{ nearest_station !== null ? nearest_station.name : "" }}
+      {{ nearest_station !== null ? nearest_station.name : "Bus Route" }}
     </v-card-title>
     <v-card-text>
       Search for bus route
@@ -43,6 +43,7 @@
 <script>
   import axios from 'axios'
   import * as hash from 'object-hash'
+  import { db } from '../../plugins/firebase'
   import { HERE_APP_ID, HERE_APP_CODE }  from '@/constants'
 
   export default {
@@ -56,10 +57,33 @@
 
     methods: {
       search () {
-          this.$router.push('/routes/' + hash(this.nearest_station.city + 
-                                              this.nearest_station.state + 
-                                              this.nearest_station.ccode + 
-                                              this.route_model))
+        let id = hash(this.nearest_station.city + 
+                      this.nearest_station.state + 
+                      this.nearest_station.ccode + 
+                      this.route_model)
+
+        // query lines info
+        axios.get('https://transit.api.here.com/v3/lines/by_stn_id.json' + 
+        '?app_id=' + HERE_APP_ID + 
+        '&app_code=' + HERE_APP_CODE +  
+        '&stnId=' + this.nearest_station.id +  
+        '&graph=1' +
+        '&max=20')
+
+        .then(res => {
+          const info = res.data.Res
+          
+          db.collection('routes').doc(id).set(info)
+          .then(docRef => {})
+          .catch(err => {})
+        })
+        .catch(err => {
+          console.log(err)
+        })
+
+
+        // redirect to the next page
+        this.$router.push('/routes/' + id)
       }
     },
 
